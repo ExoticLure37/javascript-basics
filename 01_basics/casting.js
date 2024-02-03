22e71a807c510d7a3bc6439825220fa338dab333
Aryan Maurya <127116742+ExoticLure37@users.noreply.github.com>
2024-02-03 19:38:55 +0000

    casting

---
+++ 01_basics/casting.js
@@ -0,0 +1,11 @@
+let score= "33"
+console.log(typeof score);
+
+let val = Number(score)
+console.log(typeof val);
+
+let isLoggedIn = 1
+console.log(typeof isLoggedIn);
+
+let boolIsLoggedin = Boolean(isLoggedIn)
+console.log(boolIsLoggedin);
